69ad17ba1f5eb65a14a541d393c9c2b032edb15e
Paulina Zazel <pauzazel@gmail.com>
2021-05-05 21:25:10 +0200

    ball

--- script.js
+++ script.js
@@ -1,26 +1,85 @@
+const Mosze = {
+   x: 0,
+   y: 0,
+   width: 80,
+   height: 80,
+};
+const BlueBall = {
+    x: 0,
+    y: 0,
+    width: 80,
+    height: 80,
+ };
+
+ const RedBall = {
+    x: 0,
+    y: 0,
+    width: 80,
+    height: 80,
+ };
+
 function main () {
-    let x=0;
-    let y=0;
-    const width = 80;
-    const height = 80;
+    var d = document.getElementById("Mosze");
+    d.style.width = Mosze.width+"px";
+    d.style.height = Mosze.height+"px";
+    d.style.backgroundSize = `${Mosze.width}px ${Mosze.height}px`;
+
+    d = document.getElementById("BlueBall");
+    d.style.width = BlueBall.width+"px";
+    d.style.height = BlueBall.height+"px";
+    d.style.backgroundSize = `${BlueBall.width}px ${BlueBall.height}px`;
+    BlueBall.x = Math.random () *(window.innerWidth - BlueBall.width);
+    BlueBall.y = Math.random () *(window.innerHeight - BlueBall.height);
+    d.style.left = BlueBall.x+"px";
+    d.style.top = BlueBall.y+"px";
+
+    d = document.getElementById("RedBall");
+    d.style.width = RedBall.width+"px";
+    d.style.height = RedBall.height+"px";
+    d.style.backgroundSize = `${RedBall.width}px ${RedBall.height}px`;
+    RedBall.x = Math.random () *(window.innerWidth - RedBall.width);
+    RedBall.y = Math.random () *(window.innerHeight - RedBall.height);
+    d.style.left = RedBall.x+"px";
+    d.style.top = RedBall.y+"px";
+
+
     document.onkeydown = function keydown(e) {
         switch (e.code) {
             case "ArrowLeft":
-                x = Math.max (x - 20, 0);
+                Mosze.x = Math.max (Mosze.x - 20, 0);
                 break;
             case "ArrowRight":
-                x = Math.min (x + 20, window.innerWidth - width);
+                Mosze.x = Math.min (Mosze.x + 20, window.innerWidth - Mosze.width);
                 break;
             case "ArrowUp":
-                y = Math.max (y - 20, 0);
+                Mosze.y = Math.max (Mosze.y - 20, 0);
                 break;
             case "ArrowDown":
-                y = Math.min (y + 20, window.innerHeight - height);
+                Mosze.y = Math.min (Mosze.y + 20, window.innerHeight - Mosze.height);
                 break;
         }
         var d = document.getElementById("Mosze");
-        d.style.left = x+"px";
-        d.style.top = y+"px";
+        d.style.left = Mosze.x+"px";
+        d.style.top = Mosze.y+"px";
+
+        if (Mosze.x <  BlueBall.x + BlueBall.width &&
+            Mosze.x + Mosze.width > BlueBall.x &&
+            Mosze.y < BlueBall.y + BlueBall.height &&
+            Mosze.y + Mosze.height > BlueBall.y) {
+                Mosze.height = 320;
+                Mosze.width = 320;
+        }
+
+        if (Mosze.x + (Mosze.width-80)/2 <  RedBall.x + RedBall.width &&
+            Mosze.x + (Mosze.width-80)/2 + 80 > RedBall.x &&
+            Mosze.y + (Mosze.height-80)/2 < RedBall.y + RedBall.height &&
+            Mosze.y + (Mosze.height-80)/2 + 80 > RedBall.y) {
+                Mosze.height = 80;
+                Mosze.width = 80;
+        }
+        d.style.width = Mosze.width+"px";
+        d.style.height = Mosze.height+"px";
+        d.style.backgroundSize = `${Mosze.width}px ${Mosze.height}px`;
     }
 }
 window.onload = main; 
